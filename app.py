671b7063913c6e86462e7099959af2745722143b
AndrewYW <andrewwang39@gmail.com>
2017-09-19 16:37:01 -0400

    update msg

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-import numpy
+import numpy as np
 import sys
+from random import randint
 
-index = int(sys.argv[1])
-a = numpy.zeros(shape=(index, index)
+n = int(sys.argv[1])
 
-print(a)
+def generate_array(index):
+    s = (index,index)
+
+    a = np.zeros([index,index], dtype=int)
+    for x in np.nditer(a, op_flags=['readwrite']):
+        x[...]= randint(1,3)
+
+    return a
+
+def max_step(n):
+    
+print generate_array(n)

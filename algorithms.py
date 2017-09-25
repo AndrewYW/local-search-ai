cff26614fc4c06f325f7f9267155c4f38944e9b7
Mauricio Alvarez <Mauricio_Alvarez@nwk-41-180.nwk.ruw.rutgers.edu>
2017-09-25 16:20:42 -0400

    bfs update

--- algorithms.py
+++ algorithms.py
@@ -14,50 +14,52 @@ loop:
             grab all of its neighbors 
             add them to the queue 
 """
+import queue
 
-new_matrix = matrix
-new_matrix.fill(0)
+class my_queue: 
+    def __init__(self): 
+        self.store = []
 
-def matrix_to_tree(new_matrix):
-    tree = {}
-    for node in new_matrix:
-        children = new_matrix[:] 
-        children.remove(node)
-        tree[node] = matrix_to_tree(children)
-    return tree 
+    def enqueue(self, value): 
+        self.store.append(value)
+    
+    def dequeue(self): 
+        value = None
+        try: 
+            value = self,store[0]
+            if len(self.store) == 1 :
+                self.store = []
+            else: 
+                self.store = self.store[1:]
+        except: 
+            pass 
+        return val
 
-def start_position(tree, start):
-    tree = tree[1:1]
-    return tree 
+    def IsEmpty(self): 
+        result = False
+        if len(self.store) == 0: 
+            result = True
+        return result
 
-def goal_state(tree, end):
-    if tree[end]:
-        del tree[end]
-        nodes = tree.keys()
-        if len(nodes) > 1:
-            for node in nodes: 
-                set_end(tree[node], end)
-            return tree
+path_queue = my_queue()
 
-def map_tree(tree, new_matrix, start, dist = 0):
-    for node in tree:
-        new_dist = dist + node_dist(new_matrix, start, node)
-        if tree[node]:
-            map_tree(tree[node], new_matrix, node, new_dist)
-        else: 
-            tree[node] = new_dist
-        return tree 
+def bfs(node_matrix, s, g, q):
+    temp_path = [s]
+    q.enqueue(temp_path)
+    while q.IsEmpty() == False:
+        temp_path = q.dequeue()
+        last_node = temp_path[len(temp_path) - 1]
+        if last_node == g: 
+            print ("path : ", temp_path)
+        for link_node in node_matrix[last_node]:
+            if link_node not in temp_path:
+                new_path = []
+                new_path = temp_path + [link_node]
+                q.enqueue(new_path)
 
-def node_dist(new_matrix, start, end): 
-    queue = []
-    queue.append([start])
-    while queue: 
-        path = queue.pop(0)
-        node = path[-1]
-        if node == end: 
-            return path
-        for adj in new_matrix.get(node, []):
-            new_path = list(path)
-            new_path.append(adj)
-
-    return new_matrix[start][end]
+def create_node_matrix(input_matrix, input_index):
+    node_matrix = [[Node() for i in range(input_index)] for j in range(input_index)]
+    for x in range(input_index):
+        for y in range(input_index):
+            node_matrix[x][y].steps = input_matrix[x][y]
+    return node_matrix

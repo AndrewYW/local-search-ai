af58e7449a01aeb80b12618dd8e2e7c603b35bf7
Mauricio Alvarez <Mauricio_Alvarez@Mauricios-Air.home>
2017-09-26 04:59:37 -0400

    update

--- algorithms.py
+++ algorithms.py
@@ -1,65 +1,28 @@
-"""
-make matrix into nodes 
-intiialize your empty queue 
-push start node onto the queue 
-loop:
-    node temp = q.pop()
-        check if goal node 
-            return true 
-        check if visited 
-        if yes: 
-            continue
-        if no: 
-            mark as visitied 
-            grab all of its neighbors 
-            add them to the queue 
-"""
-import queue
+import Queue
+from matrix_manip import *
 
-class my_queue: 
-    def __init__(self): 
-        self.store = []
+q = Queue.Queue() #FIFO: use get to push into back of queue and get to pull from front of queue 
 
-    def enqueue(self, value): 
-        self.store.append(value)
+def bfs(nodes): 
+    q.put(nodes[0][0])
+    nodes[0][0].depth = 0 
+    while q is not None: 
+        node = q.get()
+        node.visited = 1 
+        depth = node.depth
+        if node.children: 
+            for x in range():
+                if node.children[x].visited == 0:
+                    q.put(node.children[x])
+                    node.children[x].depth = depth + 1 
+                    
+                        
+
+
+
+
     
-    def dequeue(self): 
-        value = None
-        try: 
-            value = self,store[0]
-            if len(self.store) == 1 :
-                self.store = []
-            else: 
-                self.store = self.store[1:]
-        except: 
-            pass 
-        return val
 
-    def IsEmpty(self): 
-        result = False
-        if len(self.store) == 0: 
-            result = True
-        return result
 
-path_queue = my_queue()
 
-def bfs(node_matrix, s, g, q):
-    temp_path = [s]
-    q.enqueue(temp_path)
-    while q.IsEmpty() == False:
-        temp_path = q.dequeue()
-        last_node = temp_path[len(temp_path) - 1]
-        if last_node == g: 
-            print ("path : ", temp_path)
-        for link_node in node_matrix[last_node]:
-            if link_node not in temp_path:
-                new_path = []
-                new_path = temp_path + [link_node]
-                q.enqueue(new_path)
 
-def create_node_matrix(input_matrix, input_index):
-    node_matrix = [[Node() for i in range(input_index)] for j in range(input_index)]
-    for x in range(input_index):
-        for y in range(input_index):
-            node_matrix[x][y].steps = input_matrix[x][y]
-    return node_matrix
